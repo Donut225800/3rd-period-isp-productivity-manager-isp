00f29f9269b983ba4bf9b2867156c0602cf2df1f
samng1 <samburger.ng@gmail.com>
2025-01-17 15:40:37 +0000

    Timer.js

---
+++ broductive/src/components/Timer.js
@@ -0,0 +1,31 @@
+import React, { useState, useEffect } from "react";
+
+const Timer = () => {
+  const [seconds, setSeconds] = useState(0);
+  const [isActive, setIsActive] = useState(false);
+
+  useEffect(() => {
+    let interval = null;
+    if (isActive) {
+      interval = setInterval(() => {
+        setSeconds((seconds) => seconds + 1);
+      }, 1000);
+    } else if (!isActive && seconds !== 0) {
+      clearInterval(interval);
+    }
+    return () => clearInterval(interval);
+  }, [isActive, seconds]);
+
+  return (
+    <div>
+      <h2>Productivity Timer</h2>
+      <p>{seconds}s</p>
+      <button onClick={() => setIsActive(!isActive)}>
+        {isActive ? "Pause" : "Start"}
+      </button>
+      <button onClick={() => setSeconds(0)}>Reset</button>
+    </div>
+  );
+};
+
+export default Timer;
